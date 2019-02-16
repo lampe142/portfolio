66cd89c43397e3d4775272dd0193ac8fed0bd9e0
lampe142 <lampe142@googlemail.com>
2019-02-16 13:42:30 +0000

    integrated dashboard output new data title risk plots

--- E-Mail/send_portfolio_email.R
+++ E-Mail/send_portfolio_email.R
@@ -7,7 +7,7 @@ gmailr::mime() %>%
   gmailr::from("lampe142@googlemail.com")%>%
   gmailr::subject(paste('Portfolio Performance Max', Sys.Date()))%>%
   gmailr::html_body("attached is my portfolio performance")%>%
-  gmailr::attach_file('Dashboard/portfolio_performance.html') %>%
+  gmailr::attach_file('DashboardOutput/portfolio_performance.html') %>%
  gmailr::send_message()
 
 gmailr::mime() %>%
@@ -15,5 +15,5 @@ gmailr::mime() %>%
   gmailr::from("lampe142@googlemail.com")%>%
   gmailr::subject(paste('Portfolio Risk Max', Sys.Date()))%>%
   gmailr::html_body("attached is my portfolio")%>%
-  gmailr::attach_file('Dashboard/portfolio_risk.html') %>%
+  gmailr::attach_file('DashboardOutput/portfolio_risk.html') %>%
   gmailr::send_message()
